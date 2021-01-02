939a7d40f464059b0b6f748d46a290b427d5140c
Yuan Gao <meseta@gmail.com>
2021-01-02 17:08:18 +0000

    removed boilerplate code, added basic section

---
+++ web/src/components/Section.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card
+    class="my-4 mx-auto"
+    max-width="860"
+  >
+    <v-card-title class="primary white--text mb-5">
+      <slot name="title"></slot>
+    </v-card-title>
+
+    <v-card-text>
+      <slot></slot>
+    </v-card-text>
+
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Section extends Vue {}
+</script>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <Section>
+      <template v-slot:title>Welcome to CurateBot</template>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc faucibus nulla massa, sed porta dui pretium ut. Aenean vulputate laoreet dapibus. Vestibulum lacinia nibh at lectus semper pretium. Pellentesque semper ut ante eget finibus. Ut scelerisque velit neque. Ut porta ex orci, vitae eleifend ante pellentesque in. 
+      </p>
+    </Section>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Section from '@/components/Section.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Section
   }
 }
 </script>
